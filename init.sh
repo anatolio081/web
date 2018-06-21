e64ff3cec115e8a92b9dee694b0cca97b8f9d462
anatolio081 <anatolio081@gmail.com>
2018-06-21 19:49:58 +0300

    moreFixes

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/test
 sudo gunicorn -c /etc/gunicorn.d/hello.py hello.py:application
 sudo /etc/init.d/gunicorn restart

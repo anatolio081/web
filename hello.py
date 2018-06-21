2b8025a2549f0b2c2bb803b0a636dac2c32606f9
anatolio081 <anatolio081@gmail.com>
2018-06-21 20:00:02 +0300

    d12d12d

--- hello.py
+++ hello.py
@@ -1,12 +1,23 @@
-CONFIG = {
-    'mode': 'wsgi',
-    'working_dir': '/home/box/web',
-    # 'python': '/usr/bin/python',
-    'args': (
-        '--bind=0.0.0.0:8080',
-        '--daemon',
-        '--workers=2',
-        '--timeout=60',
-        'hello:app',
-    ),
-}
+#/usr/bin/env python3
+#from urllib.parse import parse_qs
+
+# python 2
+#from cgi import parse_qs, escape
+from re import sub
+
+def app(environ, start_response):
+    """wsgi minimal app.
+    $ gunicorn hello:app
+    """
+    #parameters = parse_qs(environ.get('QUERY_STRING', ''))
+
+    #output = ''
+    #for p in parameters:
+    #    for r in parameters[p]:
+    #        output += p + '=' + r + '\n'
+
+    # no parsing needed
+    output = sub('&', '\n', environ.get('QUERY_STRING', ''))
+
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return iter([str.encode(output)])

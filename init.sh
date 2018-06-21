bd3dde64de1f0349ae6c254b0b058c3e15676399
anatolio081 <anatolio081@gmail.com>
2018-06-21 19:54:37 +0300

    d12d21

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
+sudo ﻿ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/test
-sudo gunicorn -c /etc/gunicorn.d/hello.py hello.py:application
+sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
+﻿sudo /etc/init.d/mysql start﻿
